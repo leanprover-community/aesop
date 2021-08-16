e6d2f9fcbdcb39261470a7aa2e6add7603a6ab4a
Jannis Limperg <jannis@limperg.de>
2021-08-16 12:54:46 +0200

    Add test case for issue #2
    closes #2

---
+++ tests/1.lean
@@ -0,0 +1,10 @@
+import Aesop.Main
+
+inductive Even : Nat → Prop
+| zero : Even Nat.zero
+| plus_two {n} : Even n → Even (n + 2)
+
+attribute [aesop safe] Even.zero Even.plus_two
+
+example : Even 1000 := by
+  aesop (options { maxRuleApplications := 0, maxRuleApplicationDepth := 0 })
