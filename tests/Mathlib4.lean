c93dd9e73d05611e6b820707c611ba04180a6a1c
Jannis Limperg <jannis@limperg.de>
2022-05-11 11:22:53 +0200

    Expand Mathlib4 test
    Aesop can prove a lot more of these lemmas now.

--- tests/Mathlib4.lean
+++ tests/Mathlib4.lean
@@ -19,9 +19,7 @@ def congr_fun := @congrFun
 def congr_arg := @congrArg
 
 theorem not_of_eq_false {p : Prop} (h : p = False) : ¬p := by
-  -- TODO simp builder should support local hyps
-  -- aesop (add norm simp h)
-  simp only [h]
+  aesop (add norm simp h)
 
 -- TODO How to use a lemma like this? Maybe this is a nice example of e-matching.
 theorem cast_proof_irrel (h₁ h₂ : α = β) (a : α) : cast h₁ a = cast h₂ a := by
@@ -61,50 +59,49 @@ theorem Or.symm (h : a ∨ b) : b ∨ a := by
 def Iff.elim (f : (a → b) → (b → a) → c) (h : a ↔ b) : c := by
   aesop
 
--- TODO add Iff.intro as default rule
 theorem iff_comm : (a ↔ b) ↔ (b ↔ a) := by
-  aesop (add safe Iff.intro)
+  aesop
 
 theorem Eq.to_iff (h : a = b) : (a ↔ b) := by
-  -- aesop (add norm simp h)
-  rw [h]
-  exact Iff.rfl
+  aesop (add norm simp h)
 
-theorem neq_of_not_iff : ¬(a ↔ b) → a ≠ b := mt Eq.to_iff
+theorem neq_of_not_iff : ¬(a ↔ b) → a ≠ b :=
+  mt Eq.to_iff
 
-theorem of_iff_true (h : a ↔ True) : a := by aesop
+theorem of_iff_true (h : a ↔ True) : a := by
+  aesop
 
-theorem not_of_iff_false : (a ↔ False) → ¬a := Iff.mp
+theorem not_of_iff_false : (a ↔ False) → ¬a := by
+  aesop
 
-theorem iff_true_intro (h : a) : a ↔ True := by aesop
+theorem iff_true_intro (h : a) : a ↔ True := by
+  aesop
 
-theorem iff_false_intro (h : ¬a) : a ↔ False := by aesop
+theorem iff_false_intro (h : ¬a) : a ↔ False := by
+  aesop
 
 -- TODO proper Not handling
 theorem not_iff_false_intro (h : a) : ¬a ↔ False := by
-  -- aesop
-  simp only [h]
+  aesop (add norm simp h)
 
-theorem not_not_not : ¬¬¬a ↔ ¬a := ⟨mt not_not_intro, not_not_intro⟩
+-- TODO not_not_intro default rule?
+-- TODO how to use a rule like mt : (a → b) → ¬ b → ¬ a? We could make ¬ b an
+-- immediate argument.
+theorem not_not_not : ¬¬¬a ↔ ¬a := by
+  have h : ¬¬¬ a → ¬ a := mt not_not_intro
+  aesop (add safe [h, not_not_intro])
 
 -- TODO iff under ∀
 theorem forall_congr_iff {p q : α → Prop} (h : ∀ x, p x ↔ q x) :
     (∀ x, p x) ↔ (∀ x, q x) := by
-  apply Iff.intro
-  case mp =>
-    intros hp x
-    simp only [← h x]
-    exact hp x
-  case mpr =>
-    intros hq x
-    simp only [h x]
-    exact hq x
+  aesop (add norm simp h)
 
 theorem imp_congr_left (h : a ↔ b) : (a → c) ↔ (b → c) := by aesop
 
 -- TODO how to deal with this?
 set_option trace.aesop.steps true in
 theorem imp_congr_right (h : a → (b ↔ c)) : (a → b) ↔ (a → c) :=
+  -- aesop (add norm simp h)
   ⟨fun hab ha => (h ha).1 (hab ha), fun hcd ha => (h ha).2 (hcd ha)⟩
 
 theorem imp_congr_ctx (h₁ : a ↔ c) (h₂ : c → (b ↔ d)) : (a → b) ↔ (c → d) :=
@@ -113,28 +110,32 @@ theorem imp_congr_ctx (h₁ : a ↔ c) (h₂ : c → (b ↔ d)) : (a → b) ↔ 
 theorem imp_congr (h₁ : a ↔ c) (h₂ : b ↔ d) : (a → b) ↔ (c → d) := by
   aesop (add safe imp_congr_ctx)
 
-theorem Not.intro {a : Prop} (h : a → False) : ¬a := by aesop
+theorem Not.intro {a : Prop} (h : a → False) : ¬a := by
+  aesop
 
 -- TODO try False-elim with low priority if we have a hyp X → False in the
 -- context.
 def Not.elim (h : ¬a) (ha : a) : α := by
-  exact False.elim (h ha)
+  aesop (add norm simp h)
 
-theorem not_true : ¬True ↔ False := by aesop
+theorem not_true : ¬True ↔ False := by
+  aesop
 
-theorem not_false_iff : ¬False ↔ True := by aesop
+theorem not_false_iff : ¬False ↔ True := by
+  aesop
 
--- TODO local norm simp
 theorem not_congr (h : a ↔ b) : ¬a ↔ ¬b := by
-  -- aesop (add norm simp h)
-  simp only [h]
-  exact Iff.rfl
+  aesop (add norm simp h)
 
-theorem ne_self_iff_false (a : α) : a ≠ a ↔ False := by aesop
+theorem ne_self_iff_false (a : α) : a ≠ a ↔ False := by
+  aesop
 
-theorem eq_self_iff_true (a : α) : a = a ↔ True := by aesop
+theorem eq_self_iff_true (a : α) : a = a ↔ True := by
+  aesop
 
-theorem heq_self_iff_true (a : α) : HEq a a ↔ True := iff_true_intro HEq.rfl
+-- TODO HEq.rfl default rule
+theorem heq_self_iff_true (a : α) : HEq a a ↔ True := by
+  aesop (add safe HEq.rfl)
 
 theorem iff_not_self : ¬(a ↔ ¬a) | H => let f h := H.1 h h; f (H.2 f)
 
@@ -142,73 +143,87 @@ theorem not_iff_self : ¬(¬a ↔ a) | H => iff_not_self H.symm
 
 theorem eq_comm {a b : α} : a = b ↔ b = a := ⟨Eq.symm, Eq.symm⟩
 
-theorem And.imp (f : a → c) (g : b → d) (h : a ∧ b) : c ∧ d := ⟨f h.1, g h.2⟩
+theorem And.imp (f : a → c) (g : b → d) (h : a ∧ b) : c ∧ d := by
+  aesop
 
-theorem and_congr (h₁ : a ↔ c) (h₂ : b ↔ d) : a ∧ b ↔ c ∧ d := ⟨And.imp h₁.1 h₂.1, And.imp h₁.2 h₂.2⟩
+theorem and_congr (h₁ : a ↔ c) (h₂ : b ↔ d) : a ∧ b ↔ c ∧ d := by
+  aesop
 
-theorem and_congr_right (h : a → (b ↔ c)) : (a ∧ b) ↔ (a ∧ c) :=
-⟨fun ⟨ha, hb⟩ => ⟨ha, (h ha).1 hb⟩, fun ⟨ha, hb⟩ => ⟨ha, (h ha).2 hb⟩⟩
+-- TODO why aren't we complete in this fragment?
+theorem and_congr_right (h : a → (b ↔ c)) : (a ∧ b) ↔ (a ∧ c) := by
+  -- aesop
+  exact ⟨fun ⟨ha, hb⟩ => ⟨ha, (h ha).1 hb⟩, fun ⟨ha, hb⟩ => ⟨ha, (h ha).2 hb⟩⟩
 
-theorem and_comm : a ∧ b ↔ b ∧ a := ⟨And.symm, And.symm⟩
+theorem and_comm : a ∧ b ↔ b ∧ a := by
+  aesop
 
-theorem and_assoc : (a ∧ b) ∧ c ↔ a ∧ (b ∧ c) :=
-⟨fun ⟨⟨ha, hb⟩, hc⟩ => ⟨ha, hb, hc⟩, fun ⟨ha, hb, hc⟩ => ⟨⟨ha, hb⟩, hc⟩⟩
+theorem and_assoc : (a ∧ b) ∧ c ↔ a ∧ (b ∧ c) := by
+  aesop
 
 theorem and_left_comm : a ∧ (b ∧ c) ↔ b ∧ (a ∧ c) := by
-  rw [← and_assoc, ← and_assoc, @and_comm a b]
-  exact Iff.rfl
+  aesop
 
-theorem and_iff_left (hb : b) : a ∧ b ↔ a := ⟨And.left, fun ha => ⟨ha, hb⟩⟩
+theorem and_iff_left (hb : b) : a ∧ b ↔ a := by
+  aesop
 
-theorem and_iff_right (ha : a) : a ∧ b ↔ b := ⟨And.right, fun hb => ⟨ha, hb⟩⟩
+theorem and_iff_right (ha : a) : a ∧ b ↔ b := by
+  aesop
 
 theorem and_not_self : ¬(a ∧ ¬a) | ⟨ha, hn⟩ => hn ha
 theorem not_and_self : ¬(¬a ∧ a) | ⟨hn, ha⟩ => hn ha
 
-theorem Or.imp (f : a → c) (g : b → d) (h : a ∨ b) : c ∨ d := h.elim (inl ∘ f) (inr ∘ g)
+theorem Or.imp (f : a → c) (g : b → d) (h : a ∨ b) : c ∨ d := by
+  aesop
 
-theorem Or.imp_left (f : a → b) : a ∨ c → b ∨ c := Or.imp f id
+theorem Or.imp_left (f : a → b) : a ∨ c → b ∨ c := by
+  aesop
 
-theorem Or.imp_right (f : b → c) : a ∨ b → a ∨ c := Or.imp id f
+theorem Or.imp_right (f : b → c) : a ∨ b → a ∨ c := by
+  aesop
 
-theorem or_congr (h₁ : a ↔ c) (h₂ : b ↔ d) : (a ∨ b) ↔ (c ∨ d) :=
-⟨Or.imp h₁.1 h₂.1, Or.imp h₁.2 h₂.2⟩
+theorem or_congr (h₁ : a ↔ c) (h₂ : b ↔ d) : (a ∨ b) ↔ (c ∨ d) := by
+  aesop
 
-theorem or_comm : a ∨ b ↔ b ∨ a := ⟨Or.symm, Or.symm⟩
+theorem or_comm : a ∨ b ↔ b ∨ a := by
+  aesop
 
-theorem Or.resolve_left (h : a ∨ b) (na : ¬a) : b := h.elim na.elim id
+theorem Or.resolve_left (h : a ∨ b) (na : ¬a) : b := by
+  -- aesop
+  exact h.elim na.elim id
 
-theorem Or.neg_resolve_left (h : ¬a ∨ b) (ha : a) : b := h.elim (absurd ha) id
+theorem Or.neg_resolve_left (h : ¬a ∨ b) (ha : a) : b := by
+  -- aesop
+  exact h.elim (absurd ha) id
 
-theorem Or.resolve_right (h : a ∨ b) (nb : ¬b) : a := h.elim id nb.elim
+theorem Or.resolve_right (h : a ∨ b) (nb : ¬b) : a := by
+  -- aesop
+  exact h.elim id nb.elim
 
-theorem Or.neg_resolve_right (h : a ∨ ¬b) (nb : b) : a := h.elim id (absurd nb)
+theorem Or.neg_resolve_right (h : a ∨ ¬b) (nb : b) : a := by
+  -- aesop
+  exact h.elim id (absurd nb)
 
-open Or in
-theorem or_assoc {a b c} : (a ∨ b) ∨ c ↔ a ∨ (b ∨ c) :=
-⟨fun | inl (inl h) => inl h
-     | inl (inr h) => inr (inl h)
-     | inr h => inr (inr h),
- fun | inl h => inl (inl h)
-     | inr (inl h) => inl (inr h)
-     | inr (inr h) => inr h⟩
+theorem or_assoc {a b c} : (a ∨ b) ∨ c ↔ a ∨ (b ∨ c) := by
+  aesop
 
 theorem or_left_comm : a ∨ (b ∨ c) ↔ b ∨ (a ∨ c) := by
-  rw [← or_assoc, ← or_assoc, @or_comm a b]
-  exact Iff.rfl
+  aesop
 
 theorem not_or_intro : (na : ¬a) → (nb : ¬b) → ¬(a ∨ b) := Or.myelim
 
 theorem not_or (p q) : ¬ (p ∨ q) ↔ ¬ p ∧ ¬ q :=
 ⟨fun H => ⟨mt Or.inl H, mt Or.inr H⟩, fun ⟨hp, hq⟩ pq => pq.elim hp hq⟩
 
-theorem iff_congr (h₁ : a ↔ c) (h₂ : b ↔ d) : (a ↔ b) ↔ (c ↔ d) :=
-⟨fun h => h₁.symm.trans $ h.trans h₂, fun h => h₁.trans $ h.trans h₂.symm⟩
+theorem iff_congr (h₁ : a ↔ c) (h₂ : b ↔ d) : (a ↔ b) ↔ (c ↔ d) := by
+  aesop
 
-@[simp] theorem imp_true_iff : (α → True) ↔ True := iff_true_intro fun _ => ⟨⟩
+@[simp] theorem imp_true_iff : (α → True) ↔ True := by
+  aesop
 
-@[simp] theorem false_imp_iff : (False → a) ↔ True := iff_true_intro False.elim
+@[simp] theorem false_imp_iff : (False → a) ↔ True := by
+  aesop
 
+@[aesop norm unfold]
 def ExistsUnique (p : α → Prop) := ∃ x, p x ∧ ∀ y, p y → y = x
 
 open Lean in
@@ -223,14 +238,14 @@ theorem ExistsUnique.unique {p : α → Prop} (h : ∃! x, p x)
   {y₁ y₂ : α} (py₁ : p y₁) (py₂ : p y₂) : y₁ = y₂ :=
 let ⟨x, hx, hy⟩ := h; (hy _ py₁).trans (hy _ py₂).symm
 
-theorem Exists.imp {p q : α → Prop} (h : ∀ a, p a → q a) : (∃ a, p a) → ∃ a, q a
-| ⟨a, ha⟩ => ⟨a, h a ha⟩
+theorem Exists.imp {p q : α → Prop} (h : ∀ a, p a → q a) : (∃ a, p a) → ∃ a, q a := by
+  aesop
 
-theorem exists_congr {p q : α → Prop} (h : ∀ a, p a ↔ q a) : (∃ a, p a) ↔ ∃ a, q a :=
-⟨Exists.imp fun x => (h x).1, Exists.imp fun x => (h x).2⟩
+theorem exists_congr {p q : α → Prop} (h : ∀ a, p a ↔ q a) : (∃ a, p a) ↔ ∃ a, q a := by
+  aesop (add norm simp h)
 
-theorem exists_unique_congr {p q : α → Prop} (h : ∀ a, p a ↔ q a) : (∃! a, p a) ↔ ∃! a, q a :=
-exists_congr fun x => and_congr (h _) $ forall_congr_iff λ y => imp_congr_left (h _)
+theorem exists_unique_congr {p q : α → Prop} (h : ∀ a, p a ↔ q a) : (∃! a, p a) ↔ ∃! a, q a := by
+  aesop (add norm simp h)
 
 theorem forall_not_of_not_exists {p : α → Prop} (hne : ¬∃ x, p x) (x) : ¬p x | hp => hne ⟨x, hp⟩
 
@@ -257,7 +272,8 @@ if hp : p then h₁ hp else h₂ (h.resolve_left hp)
 @[inline] def Or.by_cases' [Decidable q] (h : p ∨ q) (h₁ : p → α) (h₂ : q → α) : α :=
 if hq : q then h₂ hq else h₁ (h.resolve_right hq)
 
-theorem Exists.nonempty {p : α → Prop} : (∃ x, p x) → Nonempty α | ⟨x, _⟩ => ⟨x⟩
+theorem Exists.nonempty {p : α → Prop} : (∃ x, p x) → Nonempty α := by
+  aesop (add safe Nonempty.intro)
 
 theorem ite_id [h : Decidable c] {α} (t : α) : (if c then t else t) = t := by cases h <;> rfl
 
@@ -310,11 +326,6 @@ theorem iff_of_eq (e : a = b) : a ↔ b := e ▸ Iff.rfl
 
 def decidable_of_iff (a : Prop) (h : a ↔ b) [D : Decidable a] : Decidable b :=
 decidable_of_decidable_of_iff h
-
-/-
-Stuff from mathlib's logic/basic.lean.
-TODO: import the whole thing.
--/
 
 theorem or_imp_distrib : (a ∨ b → c) ↔ (a → c) ∧ (b → c) :=
 ⟨fun h => ⟨fun ha => h (Or.inl ha), fun hb => h (Or.inr hb)⟩,
