4407ac1896d8acb64deaef3be79521f6e6cc444e
Jannis Limperg <jannis@limperg.de>
2022-09-22 00:34:38 +0200

    Anonymise code files

--- Aesop/Frontend/Tactic.lean
+++ Aesop/Frontend/Tactic.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2022 Jannis Limperg. All rights reserved.
+Copyright <redacted>
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jannis Limperg
+Authors: <redacted>
 -/
 
 import Aesop.Frontend.Attribute
--- Aesop/Search/Expansion/Simp/SimpGoal.lean
+++ Aesop/Search/Expansion/Simp/SimpGoal.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2022 Jannis Limperg. All rights reserved.
+Copyright <redacted>
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonardo de Moura, Jannis Limperg
+Authors: <redacted>
 -/
 
 import Aesop.Search.Expansion.Simp.Basic
