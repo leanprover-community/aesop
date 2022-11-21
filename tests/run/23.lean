c85f0cd8531e7058a4df6c8bfbf6ee05c94f4fba
Jannis Limperg <jannis@limperg.de>
2022-11-21 17:45:29 +0100

    Norm simp rules can now be defs to unfold
    This means `@[aesop norm simp]` is now morally equivalent to `@[simp]`.
    It's the first half of a fix for #23.

---
+++ tests/run/23.lean
@@ -0,0 +1,16 @@
+/-
+Copyright (c) 2022 Jannis Limperg. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jannis Limperg
+-/
+
+import Aesop
+
+def Involutive (f : α → α) : Prop :=
+  ∀ x, f (f x) = x
+
+example : Involutive not := by
+  aesop (add norm simp Involutive)
+
+example : Involutive not := by
+  aesop (add norm unfold Involutive)
