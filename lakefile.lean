c45e581f466fd7139c40707e788b92d2e23c17f5
Jannis Limperg <jannis@limperg.de>
2022-12-06 11:42:21 +0100

    Enable cloud releases

--- lakefile.lean
+++ lakefile.lean
@@ -4,9 +4,11 @@ open Lake DSL
 
 package aesop {
   precompileModules := true
+  preferReleaseBuild := true
+  buildArchive? := some "aesop"
 }
 
 @[default_target]
-lean_lib Aesop {}
+lean_lib Aesop
 
 require std from git "https://github.com/leanprover/std4" @ "main"
