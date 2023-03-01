55fc269a8a5c97f1b517924eccdea957423d36fb
Jannis Limperg <jannis@limperg.de>
2023-03-01 17:08:57 +0100

    Add test for unfold/simp builders

---
+++ tests/run/Unfold.lean
@@ -0,0 +1,28 @@
+/-
+Copyright (c) 2023 Jannis Limperg. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jannis Limperg
+-/
+
+-- Inspired by this Zulip discussion:
+-- https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Goal.20state.20not.20updating.2C.20bugs.2C.20etc.2E/near/338356062
+
+import Aesop
+
+-- TODO add to builtin rule set
+attribute [aesop safe cases] Empty
+
+structure WalkingPair
+
+def WidePullbackShape A B := Sum A B
+
+abbrev WalkingCospan : Type := WidePullbackShape Empty Empty
+
+@[aesop norm destruct]
+theorem WalkingCospan_elim : WalkingCospan → Sum Empty Empty := id
+
+example (h : WalkingCospan) : α := by
+  aesop (add norm unfold [WidePullbackShape, WalkingCospan])
+
+example (h : WalkingCospan) : α := by
+  aesop (add norm simp [WidePullbackShape, WalkingCospan])
