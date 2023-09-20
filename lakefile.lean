e34adc488c74408a02b7be79429b86f21f32295f
Peiyang-Song <p_song@ucsb.edu>
2023-09-20 07:08:03 -0700

    Bump to LeanInfer v0.0.7

--- lakefile.lean
+++ lakefile.lean
@@ -4,6 +4,7 @@ open Lake DSL
 
 package aesop {
   precompileModules := false -- breaks mathlib cache
+  moreLinkArgs := #["-L./lake-packages/LeanInfer/build/lib", "-lonnxruntime", "-lstdc++"]
 }
 
 @[default_target]
@@ -14,4 +15,4 @@ lean_lib AesopTest {
 }
 
 require std from git "https://github.com/leanprover/std4" @ "main"
-require LeanInfer from git "https://github.com/lean-dojo/LeanInfer.git" @ "v0.0.4"
+require LeanInfer from git "https://github.com/lean-dojo/LeanInfer.git" @ "v0.0.7"
