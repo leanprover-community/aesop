dd168733888ce5047ca0da7831e356f178c25983
Jannis Limperg <jannis@limperg.de>
2023-09-22 10:03:55 +0200

    Add ext builtin rule

---
+++ Aesop/BuiltinRules/Ext.lean
@@ -0,0 +1,30 @@
+/-
+Copyright (c) 2023 Jannis Limperg. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jannis Limperg
+-/
+
+import Aesop.Frontend.Attribute
+
+namespace Aesop.BuiltinRules
+
+open Lean Lean.Meta
+
+def unhygienicExt (goal : MVarId) : MetaM (Array MVarId) :=
+  unhygienic do
+    let (_, subgoals) ←
+      Std.Tactic.Ext.extCore goal [] (failIfUnchanged := true) |>.run' {}
+    return subgoals.map (·.fst)
+
+def unhygienicExtWithScript (goal : MVarId) (generateScript : Bool) :
+    MetaM (Array MVarId × Option RuleTacScriptBuilder) := do
+  let subgoals ← unhygienicExt goal
+  let scriptBuilder? :=
+    mkScriptBuilder? generateScript (.unhygienicExt subgoals.size)
+  return (subgoals, scriptBuilder?)
+
+@[aesop safe -50 (rule_sets [builtin])]
+def ext : RuleTac := RuleTac.ofSingleRuleTac λ input =>
+  unhygienicExtWithScript input.goal input.options.generateScript
+
+end Aesop.BuiltinRules
