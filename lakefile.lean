100fcd3f829d78ef12fd690d6fcec46ceff4d0c5
Peiyang-Song <p_song@ucsb.edu>
2023-09-27 21:14:36 -0700

    Increase number of candidate sequences to 64

--- lakefile.lean
+++ lakefile.lean
@@ -15,4 +15,4 @@ lean_lib AesopTest {
 }
 
 require std from git "https://github.com/leanprover/std4" @ "main"
-require LeanInfer from git "https://github.com/lean-dojo/LeanInfer.git" @ "v0.0.7"
+require LeanInfer from git "https://github.com/lean-dojo/LeanInfer.git" @ "peiyang-dev"
