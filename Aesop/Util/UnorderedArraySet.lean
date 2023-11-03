cb87803274405db79ec578fc07c4730c093efb90
Scott Morrison <scott.morrison@gmail.com>
2023-11-03 22:13:12 +1100

    chore: bump Std dependency (#84)

--- Aesop/Util/UnorderedArraySet.lean
+++ Aesop/Util/UnorderedArraySet.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Jannis Limperg. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jannis Limperg
 -/
-
+import Std.Data.Array.Basic
 import Std.Data.Array.Merge
 
 open Lean
