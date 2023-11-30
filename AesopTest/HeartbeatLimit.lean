b210bf0079cb53eb95599f09048aa8da9e3a85d5
Scott Morrison <scott.morrison@gmail.com>
2023-11-30 14:13:15 +1100

    fail_if_success can no longer catch timeout errors

--- AesopTest/HeartbeatLimit.lean
+++ AesopTest/HeartbeatLimit.lean
@@ -5,17 +5,24 @@ Authors: Jannis Limperg
 -/
 
 import Aesop
+import Std.Tactic.GuardMsgs
 
 set_option aesop.check.all true
+set_option linter.unreachableTactic false
 
 @[aesop safe constructors]
 inductive Even : Nat → Prop
   | zero : Even 0
   | plusTwo : Even n → Even (n + 2)
 
+/--
+error: (deterministic) timeout at 'whnf', maximum number of heartbeats (1) has been reached (use 'set_option maxHeartbeats <num>' to set the limit)
+-/
+#guard_msgs in
 example : Even 10 := by
-  fail_if_success
-    aesop (options := { maxRuleHeartbeats := 1, terminal := true })
+  aesop (options := { maxRuleHeartbeats := 1, terminal := true })
+
+example : Even 10 := by
   aesop
 
 example (n m k : Nat) : n + m + k = (n + m) + k := by
