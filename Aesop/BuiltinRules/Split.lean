18de6dec7579c2957849102dc5ac9f915d73c2a0
Jannis Limperg <jannis@limperg.de>
2024-05-31 20:52:43 +0200

    BuiltinRules.Split: remove aesop_split_hyps tactic
    The `splitHypotheses` rule now generates `split at h` script steps.

--- Aesop/BuiltinRules/Split.lean
+++ Aesop/BuiltinRules/Split.lean
@@ -11,50 +11,72 @@ open Lean.Meta
 
 namespace Aesop.BuiltinRules
 
+private def renameInaccessibleFVars (goals : Array MVarId)
+    (generateScript : Bool) :
+    MetaM (Array MVarId × Array RuleTacScriptBuilder) := do
+  let mut scriptBuilders := Array.mkEmpty goals.size
+  let mut newGoals := Array.mkEmpty goals.size
+  for goal in goals do
+    let (goal, _, scriptBuilder?) ←
+      renameInaccessibleFVarsWithScript goal generateScript
+    newGoals := newGoals.push goal
+    if let some scriptBuilder := scriptBuilder? then
+      scriptBuilders := scriptBuilders.push scriptBuilder
+  return (newGoals, scriptBuilders)
+
 @[aesop (rule_sets := [builtin]) safe 100]
 def splitTarget : RuleTac := RuleTac.ofSingleRuleTac λ input => do
   let generateScript := input.options.generateScript
   let (some goals) ← splitTarget? input.goal | throwError
     "nothing to split in target"
   let goals := goals.toArray
-  let mut renameScriptBuilders := Array.mkEmpty goals.size
-  let mut renamedGoals := Array.mkEmpty goals.size
-  for goal in goals do
-    let (goal, _, scriptBuilder?) ←
-      renameInaccessibleFVarsWithScript goal generateScript
-    renamedGoals := renamedGoals.push goal
-    if let some scriptBuilder := scriptBuilder? then
-      renameScriptBuilders := renameScriptBuilders.push scriptBuilder
+  let (renamedGoals, renameScriptBuilders) ←
+    renameInaccessibleFVars goals generateScript
   let scriptBuilder? :=
     mkScriptBuilder? input.options.generateScript $
       ScriptBuilder.ofTactic goals.size `(tactic| split)
         |>.seq renameScriptBuilders
   return (renamedGoals, scriptBuilder?, none)
 
-def splitFirstHypothesis (goal : MVarId) : MetaM (Option (Array MVarId)) :=
+def splitFirstHypothesis (goal : MVarId) (generateScript : Bool) :
+    MetaM (Option (Array MVarId × Option RuleTacScriptBuilder)) :=
   goal.withContext do
     for ldecl in ← getLCtx do
       if ! ldecl.isImplementationDetail then
         if let some goals ← splitLocalDecl? goal ldecl.fvarId then
-          return goals.toArray
+          let goals := goals.toArray
+          let (renamedGoals, renameScriptBuilders) ←
+            renameInaccessibleFVars goals generateScript
+          let scriptBuilder? := mkScriptBuilder? generateScript $
+            ScriptBuilder.ofTactic goals.size
+              `(tactic| split at $(mkIdent ldecl.userName):ident)
+              |>.seq renameScriptBuilders
+          return (renamedGoals, scriptBuilder?)
     return none
 
-def splitHypothesesCore (goal : MVarId) : MetaM (Option (Array MVarId)) :=
-  saturate1 goal splitFirstHypothesis
-
-elab "aesop_split_hyps" : tactic =>
-  Elab.Tactic.liftMetaTactic λ goal => do
-    match ← splitHypothesesCore goal with
-    | none => throwError "no splittable hypothesis found"
-    | some goals => return goals.toList
+partial def splitHypothesesCore (goal : MVarId) (generateScript : Bool) :
+    MetaM (Option (Array MVarId × Option RuleTacScriptBuilder)) :=
+  withIncRecDepth do
+  let some (goals, scriptBuilder?) ← splitFirstHypothesis goal generateScript
+    | return none
+  let mut subgoals := #[]
+  let mut subgoalScriptBuilders := #[]
+  for g in goals do
+    let sub? ← splitHypothesesCore g generateScript
+    if let some (subgoals', subgoalScriptBuilder?) := sub? then
+      subgoals := subgoals ++ subgoals'
+      if let some subgoalScriptBuilder := subgoalScriptBuilder? then
+        subgoalScriptBuilders := subgoalScriptBuilders.push subgoalScriptBuilder
+    else
+      subgoals := subgoals.push g
+  let scriptBuilder? := return (← scriptBuilder?).seq subgoalScriptBuilders
+  return (subgoals, scriptBuilder?)
 
 @[aesop (rule_sets := [builtin]) safe 1000]
 def splitHypotheses : RuleTac := RuleTac.ofSingleRuleTac λ input => do
-  let (some goals) ← splitHypothesesCore input.goal | throwError
-    "no splittable hypothesis found"
-  let scriptBuilder? :=
-    mkScriptBuilder? input.options.generateScript $
-      .ofTactic goals.size `(tactic| aesop_split_hyps)
+  let (some (goals, scriptBuilder?)) ←
+    splitHypothesesCore input.goal input.options.generateScript
+    | throwError "no splittable hypothesis found"
   return (goals, scriptBuilder?, none)
 
 end Aesop.BuiltinRules
--- AesopTest/SplitScript.lean
+++ AesopTest/SplitScript.lean
@@ -24,3 +24,27 @@ warning: declaration uses 'sorry'
 example {A B : Prop} : if P then A else B := by
   aesop? (config := { warnOnNonterminal := false })
   all_goals sorry
+
+/--
+info: Try this:
+  split at h <;> rename_i h_1
+  · simp_all only [true_or]
+  · simp_all only [or_true]
+-/
+#guard_msgs in
+example (h : if P then A else B) : A ∨ B := by
+  aesop?
+
+/--
+info: Try this:
+  split at h <;> [rename_i n; rename_i n; rename_i n_1 x x_1]
+  · simp_all only [true_or]
+  · simp_all only [true_or, or_true]
+  · simp_all only [imp_false, or_true]
+-/
+#guard_msgs in
+theorem foo (n : Nat) (h : match n with | 0 => A | 1 => B | _ => C) :
+    A ∨ B ∨ C := by
+  set_option aesop.check.rules false in -- TODO
+  set_option aesop.check.tree false in
+  aesop?
