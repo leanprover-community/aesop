0a5ecbea354ad07f840ebef01c427abfd260ef05
Jannis Limperg <jannis@limperg.de>
2024-06-13 18:19:57 +0200

    Script.StructureStatic: refactor to use orderedUScriptToSScript
    The static structuring algorithm previously went directly from `UScript`
    to `SScript`. Now it merely reorders the `UScript` and uses
    `orderedUScriptToSScript` to produce the `SScript` (like the dynamic
    structuring algorithm). This fixes some edge cases and simplifies the
    algorithm a lot.

--- Aesop/Script/StructureStatic.lean
+++ Aesop/Script/StructureStatic.lean
@@ -1,14 +1,12 @@
-import Aesop.Script.SScript
-import Aesop.Script.UScript
+import Aesop.Script.UScriptToSScript
 import Aesop.Script.Util
-import Aesop.Tracing
 
 open Lean Lean.Meta
 
 namespace Aesop.Script
 
-partial def UScript.toSScriptStatic (tacticState : TacticState)
-    (script : UScript) : CoreM SScript :=
+partial def structureStaticCore (tacticState : TacticState) (script : UScript) :
+    CoreM UScript :=
   withConstAesopTraceNode .debug (return m!"statically structuring the tactic script") do
   aesop_trace[debug] "unstructured script:{indentD $ MessageData.joinSep (script.toList.map λ step => m!"{step}") "\n"}"
   let mut steps : HashMap MVarId (Nat × Step) := mkHashMap script.size
@@ -18,43 +16,34 @@ partial def UScript.toSScriptStatic (tacticState : TacticState)
       if step.postGoals[0].goal == step.preGoal then
         continue
     steps := steps.insert step.preGoal (i, step)
-  (·.fst) <$> go steps tacticState
+  (·.fst.toArray) <$> go steps tacticState
 where
   go (steps : HashMap MVarId (Nat × Step)) (tacticState : TacticState) :
-      CoreM (SScript × TacticState) :=
+      CoreM (List Step × TacticState) :=
     withIncRecDepth do
-    if tacticState.visibleGoals.isEmpty then
-      return (.empty, tacticState)
-    else if tacticState.visibleGoals.size == 1 ||
-            tacticState.visibleGoalsHaveMVars then
-      -- "Unstructured mode"
-      -- TODO If the original order happens to solve the main goal, we can
-      -- structure opportunistically.
-      let firstStep? :=
-        findFirstStep? tacticState.visibleGoals (steps.find? ·.goal) (·.fst)
-      if let some (goalPos, _, _, firstStep) := firstStep? then
-        aesop_trace[debug] "rendering step:{indentD $ toMessageData firstStep}"
-        let tacticState ← tacticState.applyStep firstStep
-        let (tailScript, tacticState) ← go steps tacticState
-        return (.onGoal goalPos firstStep tailScript, tacticState)
-      else
-        throwError "aesop: internal error while structuring script: no script step found for any of the goals {tacticState.visibleGoals.map (·.goal.name)}"
+    if let some goal := tacticState.visibleGoals[0]? then
+      let step ← nextStep steps tacticState goal
+      aesop_trace[debug] "rendering step:{indentD $ toMessageData step}"
+      let tacticState ← tacticState.applyStep step
+      let (tailScript, tacticState) ← go steps tacticState
+      return (step :: tailScript, tacticState)
     else
-      -- "Structured mode"
-      let mut tacticState := tacticState
-      let mut nestedScripts := Array.mkEmpty tacticState.visibleGoals.size
-      -- The following loop is not equivalent to a for loop because some of
-      -- the later visible goals may be solved while solving an earlier visible
-      -- goal.
-      while h : tacticState.visibleGoals.size > 0 do
-        let goal := tacticState.visibleGoals[0]
-        let (nestedScript, nestedTacticState) ←
-          withConstAesopTraceNode .debug (return m!"visiting main goal: {goal.goal.name}") do
-            tacticState.onGoalM goal.goal λ tacticState => do
-              go steps tacticState
-        nestedScripts := nestedScripts.push nestedScript
-        tacticState := nestedTacticState
-      let script := nestedScripts.foldr (init := .empty) (.focusAndSolve 0)
-      return (script, tacticState)
+      return ([], tacticState)
+
+  nextStep (steps : HashMap MVarId (Nat × Step)) (tacticState : TacticState)
+      (mainGoal : GoalWithMVars) : CoreM Step := do
+    if mainGoal.mvars.isEmpty then
+      if let some (_, step) := steps[mainGoal.goal] then
+        return step
+    let firstStep? :=
+      findFirstStep? tacticState.visibleGoals (steps[·.goal]) (·.fst)
+    let some (_, _, _, firstStep) := firstStep?
+      | throwError "aesop: internal error while structuring script: no script step found for any of the goals {tacticState.visibleGoals.map (·.goal.name)}"
+    return firstStep
+
+def UScript.toSScriptStatic (tacticState : TacticState) (uscript : UScript) :
+    CoreM SScript := do
+  let script ← structureStaticCore tacticState uscript
+  orderedUScriptToSScript script tacticState
 
 end Aesop.Script
