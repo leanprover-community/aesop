7cb721139adaeed4c38a573a0a8f0b94b5ba9c4c
Jannis Limperg <jannis@limperg.de>
2024-09-30 13:18:43 +0200

    Move ElabRuleTerm from Builder.Basic to RuleTac.Basic

--- Aesop/Builder/Basic.lean
+++ Aesop/Builder/Basic.lean
@@ -115,32 +115,4 @@ def elabInductiveRuleIdent (builderName : BuilderName) (term : Term) :
   else
     throwError "aesop: {builderName} builder: expected '{term}' to be an inductive type or structure"
 
-inductive ElabRuleTerm
-  | const (decl : Name)
-  | term (term : Term) (expr : Expr)
-
-namespace ElabRuleTerm
-
-def expr : ElabRuleTerm → MetaM Expr
-  | const decl => mkConstWithFreshMVarLevels decl
-  | term _ e => return e
-
-def scope : ElabRuleTerm → ScopeName
-  | const .. => .global
-  | term .. => .local
-
-def name : ElabRuleTerm → MetaM Name
-  | const decl => return decl
-  | term _ e => getRuleNameForExpr e
-
-def toRuleTerm : ElabRuleTerm → RuleTerm
-  | const decl => .const decl
-  | term t _ => .term t
-
-def ofElaboratedTerm (tm : Term) (expr : Expr) : ElabRuleTerm :=
-  if let some decl := expr.constName? then
-    .const decl
-  else
-    .term tm expr
-
-end Aesop.ElabRuleTerm
+end Aesop
