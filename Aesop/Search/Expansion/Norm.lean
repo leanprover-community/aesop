6f49b47615866c2049350d9a583cf94f5f8f232c
Kim Morrison <kim@tqft.net>
2024-10-19 18:06:08 +1100

    chore: adaptations for nightly-2024-10-17 (#168)
    Co-authored-by: Jannis Limperg <jannis@limperg.de>

--- Aesop/Search/Expansion/Norm.lean
+++ Aesop/Search/Expansion/Norm.lean
@@ -230,12 +230,11 @@ def normSimp (goal : MVarId) (goalMVars : Std.HashSet MVarId) :
     NormM (Option NormRuleResult) := do
   profilingRule .normSimp (wasSuccessful := λ _ => true) do
     checkSimp "norm simp" (mayCloseGoal := true) goal do
-      try
-        withNormTraceNode .normSimp do
+      tryCatchRuntimeEx
+        (withNormTraceNode .normSimp do
           withMaxHeartbeats (← read).options.maxSimpHeartbeats do
-            normSimpCore goal goalMVars
-      catch e =>
-        throwError "aesop: error in norm simp: {e.toMessageData}"
+            normSimpCore goal goalMVars)
+        (λ e => throwError "aesop: error in norm simp: {e.toMessageData}")
 
 def normUnfoldCore (goal : MVarId) : NormM (Option NormRuleResult) := do
   let unfoldRules := (← read).ruleSet.unfoldRules
@@ -250,12 +249,11 @@ def normUnfoldCore (goal : MVarId) : NormM (Option NormRuleResult) := do
 def normUnfold (goal : MVarId) : NormM (Option NormRuleResult) := do
   profilingRule .normUnfold (wasSuccessful := λ _ => true) do
     checkSimp "unfold simp" (mayCloseGoal := false) goal do
-      try
-        withNormTraceNode .normUnfold do
+      tryCatchRuntimeEx
+        (withNormTraceNode .normUnfold do
           withMaxHeartbeats (← read).options.maxUnfoldHeartbeats do
-            normUnfoldCore goal
-      catch e =>
-        throwError "aesop: error in norm unfold: {e.toMessageData}"
+            normUnfoldCore goal)
+        (λ e => throwError "aesop: error in norm unfold: {e.toMessageData}")
 
 inductive NormSeqResult where
   | proved (script : Array (DisplayRuleName × Option (Array Script.LazyStep)))
