97570853fc7b3225dcb147486814f7e760a2cc96
Xavier Genereux <xaviergenereux@hotmail.com>
2025-02-11 13:55:53 +0100

    fix authors trans

--- AesopTest/Forward/SynthTrans.lean
+++ AesopTest/Forward/SynthTrans.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Jannis Limperg. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jannis Limperg
+Authors: Jannis Limperg, Xavier Généreux
 -/
 
 import Aesop
