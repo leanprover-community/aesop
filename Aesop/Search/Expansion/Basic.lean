76a782b161da3faacc43aa5471bbd2e1d262970a
Eric Wieser <wieser.eric@gmail.com>
2025-05-21 19:11:40 +0000

    chore: use `Except` instead of `Sum` (#212)
    Co-authored-by: Jannis Limperg <limperg@amazon.com>
    Co-authored-by: Jannis Limperg <jannis@limperg.de>

--- Aesop/Search/Expansion/Basic.lean
+++ Aesop/Search/Expansion/Basic.lean
@@ -13,15 +13,15 @@ namespace Aesop
 
 def runRuleTac (tac : RuleTac) (ruleName : RuleName)
     (preState : Meta.SavedState) (input : RuleTacInput) :
-    BaseM (Sum Exception RuleTacOutput) := do
+    BaseM (Except Exception RuleTacOutput) := do
   let result ←
     tryCatchRuntimeEx
-      (Sum.inr <$> runInMetaState preState do
+      (Except.ok <$> runInMetaState preState do
         withAtMostMaxHeartbeats input.options.maxRuleHeartbeats do
           tac input)
-      (λ e => return Sum.inl e)
+      (λ e => return .error e)
   if ← Check.rules.isEnabled then
-    if let (Sum.inr ruleOutput) := result then
+    if let .ok ruleOutput := result then
       ruleOutput.applications.forM λ rapp => do
         if let (some err) ← rapp.check input then
           throwError "{Check.rules.name}: while applying rule {ruleName}: {err}"
