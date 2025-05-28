22db70895e4f3c0669c236f688005044fb9d1ec6
Jannis Limperg <limperg@amazon.com>
2025-05-28 14:29:51 +0200

    Forward.State: fix a bug where matches with erased hyps were not removed from the forward state

---
+++ AesopTest/ForwardUnknownFVar.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2025 Xavier Généreux. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Xavier Généreux, Jannis Limperg
+-/
+
+import Aesop
+
+attribute [aesop safe forward 1] Nat.le_trans
+attribute [-aesop] Aesop.BuiltinRules.applyHyps
+
+variable (a b c d e f : Nat)
+
+/-- error: tactic 'aesop' failed, failed to prove the goal after exhaustive search. -/
+#guard_msgs in
+set_option aesop.smallErrorMessages true in
+example (h₃ : c ≤ d) (h₄ : d ≤ e) (h₅ : e ≤ f) (h : a = f) : False := by
+  aesop (config := { terminal := true })
