345f08919eb57435b621203071264679c6b0ac1a
Sebastian Ullrich <sebasti@nullri.ch>
2025-11-05 13:44:19 +0100

    fix

--- Aesop/Frontend.lean
+++ Aesop/Frontend.lean
@@ -3,4 +3,5 @@ module
 public import Aesop.Frontend.Attribute
 public import Aesop.Frontend.Command
 public import Aesop.Frontend.RuleExpr
+public import Aesop.Frontend.Saturate
 public import Aesop.Frontend.Tactic
