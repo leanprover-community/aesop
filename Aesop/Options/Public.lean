15a3c9d3b276b4fe0c2a742d2d6a9f42f9bf490b
Jannis Limperg <limperg@amazon.com>
2025-11-20 20:34:38 +0100

    Add aesop.stats.file option that exports stats to a JSONL file

--- Aesop/Options/Public.lean
+++ Aesop/Options/Public.lean
@@ -192,4 +192,20 @@ register_option aesop.warn.nonterminal : Bool := {
   defValue := true
 }
 
+/--
+(aesop) Collect statistics about Aesop invocations.
+-/
+register_option aesop.collectStats : Bool := {
+  descr := "(aesop) Collect statistics about Aesop invocations. Use #aesop_stats to display the collected statistics."
+  defValue := false
+}
+
+/--
+(aesop) Write statistics about Aesop invocations to the given file in JSONL format.
+-/
+register_option aesop.stats.file : String := {
+  descr := "(aesop) Write statistics about Aesop invocations to the given file in JSONL format. Each invocation adds one JSON record to the file."
+  defValue := ""
+}
+
 end Aesop
